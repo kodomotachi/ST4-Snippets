fccd1d560de1f658a6ab2b3fb34a009f311b91df
Đào Lê Bảo Minh <74275726+KozerG17@users.noreply.github.com>
2021-09-05 16:18:47 +0700

    Update and rename c++_template.cpp to cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,4 +1,7 @@
-#ifdef LOCAL
+// Compilability: C++11 or above.
+// Source: https://github.com/KozerG17/ST4-Snippets/blob/main/cpp.cpp
+
+#ifdef LBM_LOCAL
 #include <local.h>
 #else
 #include <bits/stdc++.h>
@@ -8,16 +11,15 @@
 // libraries
 using namespace std;
 namespace std {
-	// definitions
+	// macros
 	#define _ first
 	#define __ second
-	#define Read Reader(),
-	#define Gint Reader::getData<i64>()
-	#define Gstr Reader::getData<str>()
 	#define all(a)  (a).begin(),  (a).end()
 	#define rall(a) (a).rbegin(), (a).rend()
 	#define For(i, u, v) for (int i = (u), _n_ = (v); i < _n_; i++)
 	#define Rof(i, u, v) for (int i = (u), _n_ = (v); --i >= _n_; )
+
+	// typedefs
 	using i64 = long long;              using str = string;
 	using u64 = unsigned long long;     using dbl = long double;
 	using p32 = pair<int, int>;         using p64 = pair<i64, i64>;
@@ -35,17 +37,18 @@ namespace std {
 	constexpr dbl pi  = acos(-1),   eps = 1e-7;
 	constexpr i64 md  = 1e9 + 7,    _md = 1e9 + 1617;
 
-	// structures
-	struct Reader {
+	// devices
+	class Reader {
+	public:
 		template<class T> Reader &operator,(T &x) {
 			return ok &= !!(cin >> x), *this;
 		} bool ok = true; operator bool() { return ok; }
 		template<class T> static T getData() { T x; return cin >> x, x; }
+	private:
+		#define Read Reader(),
+		#define Gint Reader::getData<i64>()
+		#define Gstr Reader::getData<str>()
 	};
-	template<class... Args> auto V(size_t n, Args&&... args) {
-		if constexpr(sizeof...(args) == 1) return vector(n, args...);
-		else return vector(n, V(args...));
-	}
 }
 
 
@@ -75,5 +78,5 @@ int main() {
 
 
 // ------------------------------------------                    |\__/,|   (`\
-// | author: dlbm1302 | created: 17.07.2021 |                  _.|o o  |_   ) )
+// | author: dlbm1302 | created: 05.09.2021 |                  _.|o o  |_   ) )
 // -----------------------------------------------------------(((---(((------------
